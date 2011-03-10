13d1001b99d1780be241dfb0744ae1728ec957cc
Adam Duke <adam.v.duke@gmail.com>
2011-03-10 01:44:59 -0500

    fixed the CatchAllExceptionMapper to build a response based on the incoming exception

--- DoWhatNowServices/src/main/java/com/adamvduke/dowhatnow/resources/exception/mapper/CatchAllExceptionMapper.java
+++ DoWhatNowServices/src/main/java/com/adamvduke/dowhatnow/resources/exception/mapper/CatchAllExceptionMapper.java
@@ -1,22 +1,39 @@
 package com.adamvduke.dowhatnow.resources.exception.mapper;
 
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class CatchAllExceptionMapper implements ExceptionMapper <java.lang.Exception> {
+public class CatchAllExceptionMapper implements ExceptionMapper <WebApplicationException> {
+
+	private static final String formatMessage = "{\"request\":\"%s\",\"error\":\"%s\"}";
+	private static final String defaultMessage = "Check http response headers.";
 
 	@Context
 	UriInfo uriInfo;
 
 	@Override
-	public Response toResponse( Exception exception ) {
+	public Response toResponse( WebApplicationException exception ) {
 
-		String message = "{\"request\":\"" + uriInfo.getPath() + "\",\"error\":\"" + exception.getMessage() + "\"}";
-		return Response.status( 400 ).entity( message ).type( MediaType.APPLICATION_JSON ).build();
+		Response jerseyResponse = exception.getResponse();
+		ResponseBuilder customResponseBuilder = Response.status( jerseyResponse.getStatus() );
+		MultivaluedMap <String, Object> map = jerseyResponse.getMetadata();
+		for ( String key : map.keySet() ) {
+			customResponseBuilder.header( key, map.get( key ) );
+		}
+		String exceptionMessage = exception.getMessage();
+		if ( exceptionMessage == null ) {
+			exceptionMessage = defaultMessage;
+		}
+		customResponseBuilder.entity( String.format( formatMessage, uriInfo.getPath(), exceptionMessage ) );
+		customResponseBuilder.type( MediaType.APPLICATION_JSON );
+		return customResponseBuilder.build();
 	}
 }
