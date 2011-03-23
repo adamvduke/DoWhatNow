26b9b2867df32c29f7e0d65ec684e8c21bd8d68b
Adam Duke <adam.v.duke@gmail.com>
2011-03-23 16:55:46 -0400

    made the OAuthFilter only filter requests to protected resources
    the oauth filter now has a List<String> that contains the prefixes for any protected
    resources. Each request is checked to determine if it is attempting to access a protected
    resource before checking to make sure the request is properly signed.

--- AppEngine_Common/src/test/java/com/adamvduke/appengine/common/filter/OAuthFilterTest.java
+++ AppEngine_Common/src/test/java/com/adamvduke/appengine/common/filter/OAuthFilterTest.java
@@ -1,7 +1,12 @@
 package com.adamvduke.appengine.common.filter;
 
+import static org.junit.Assert.fail;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -13,28 +18,64 @@ import com.sun.jersey.spi.container.ContainerRequest;
 
 public class OAuthFilterTest {
 
+	@Test
+	public void shouldForwardRequestIfNoProtectedResources() throws OAuthRequestException {
+
+		OAuthService oauthService = mock( OAuthService.class );
+		ContainerRequest request = mock( ContainerRequest.class );
+
+		// given
+		OAuthFilter filter = new OAuthFilter();
+		filter.setOauthService( oauthService );
+
+		// when
+		ContainerRequest forwardedRequest = filter.filter( request );
+
+		// then
+		Assert.assertSame( request, forwardedRequest );
+	}
+
+	@Test
+	public void shouldForwardRequestIfNotAccessingProtectedResource() throws OAuthRequestException {
+
+		OAuthService oauthService = mock( OAuthService.class );
+		ContainerRequest request = mock( ContainerRequest.class );
+
+		// given
+		given( request.getPath() ).willReturn( "/somePath/something.html" );
+		List <String> protectedResourcePrefixes = new ArrayList <String>();
+		protectedResourcePrefixes.add( "/protected" );
+		OAuthFilter filter = new OAuthFilter();
+		filter.setOauthService( oauthService );
+		filter.setProtectedResourcePrefixes( protectedResourcePrefixes );
+
+		// when
+		ContainerRequest forwardedRequest = filter.filter( request );
+
+		// then
+		Assert.assertSame( request, forwardedRequest );
+	}
+
 	@Test( expected = UnauthorizedRequestException.class )
 	public void shouldThrowUnauthorizedException() throws OAuthRequestException {
 
 		OAuthService oauthService = mock( OAuthService.class );
 		ContainerRequest request = mock( ContainerRequest.class );
 
+		// given
+		given( request.getPath() ).willReturn( "/somePath/something.html" );
 		given( oauthService.getCurrentUser() ).willThrow( new OAuthRequestException( "" ) );
-
+		List <String> protectedResourcePrefixes = new ArrayList <String>();
+		protectedResourcePrefixes.add( "/somePath/" );
 		OAuthFilter filter = new OAuthFilter();
 		filter.setOauthService( oauthService );
+		filter.setProtectedResourcePrefixes( protectedResourcePrefixes );
+
+		// when
 		filter.filter( request );
-	}
 
-	@Test
-	public void shouldForwardRequest() throws OAuthRequestException {
-
-		OAuthService oauthService = mock( OAuthService.class );
-		ContainerRequest request = mock( ContainerRequest.class );
-
-		OAuthFilter filter = new OAuthFilter();
-		filter.setOauthService( oauthService );
-		ContainerRequest forwardedRequest = filter.filter( request );
-		Assert.assertSame( request, forwardedRequest );
+		// then
+		// shouldn't get here
+		fail();
 	}
 }
